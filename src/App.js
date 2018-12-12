9c3ef2641f585a8adab26283033c83393719a1ff
virdginiya1903 <virdginiya1903@mail.ru>
2018-12-12 19:39:16 +0300

    add method map in app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React, { Component } from "react";
 import "./App.css";
 import Content from "./Content";
-const IMG_DATA = {
-  BearGuardian:
-    "http://dl3.joxi.net/drive/2018/12/08/0027/3308/1830124/24/73d83f8971.jpg",
-  Fish:
-    "http://dl3.joxi.net/drive/2018/12/08/0027/3308/1830124/24/b6fefd6f9b.jpg",
-  BigBar:
-    "http://dl3.joxi.net/drive/2018/12/08/0027/3308/1830124/24/7c7a9fe143.jpg",
-  Fuhrmann:
-    "http://dl3.joxi.net/drive/2018/12/08/0027/3308/1830124/24/fceb51ffef.jpg"
-};
+
+const IMG_DATA = [
+  {
+    name: "BearGuardian",
+    url:
+      "http://dl3.joxi.net/drive/2018/12/08/0027/3308/1830124/24/73d83f8971.jpg"
+  },
+  {
+    name: "Fish",
+    url:
+      "http://dl3.joxi.net/drive/2018/12/08/0027/3308/1830124/24/b6fefd6f9b.jpg"
+  },
+  {
+    name: "BigBar",
+    url:
+      "http://dl3.joxi.net/drive/2018/12/08/0027/3308/1830124/24/7c7a9fe143.jpg"
+  },
+  {
+    name: "Fuhrmann",
+    url:
+      "http://dl3.joxi.net/drive/2018/12/08/0027/3308/1830124/24/fceb51ffef.jpg"
+  }
+];
+
 class App extends Component {
   state = {
-    constellation: " Bear-guardian",
-    img: IMG_DATA["BearGuardian"]
+    name: "BearGuardian",
+    url:
+      "http://dl3.joxi.net/drive/2018/12/08/0027/3308/1830124/24/73d83f8971.jpg"
   };
+
   myFunc = e => {
     this.setState({
-      constellation: e.currentTarget.textContent,
-      img: IMG_DATA[e.currentTarget.dataset.constellation]
+      name: e.target.name,
+      url: e.currentTarget.dataset.url
     });
+    console.log(e.target.url);
   };
 
   render() {
@@ -28,37 +45,23 @@ class App extends Component {
       <div className="App ">
         <div className="navbar navbar-expand-lg navbar-light bg-light">
           <div className="container">
-            <button
-              className="btn btn-secondary"
-              onClick={this.myFunc}
-              data-constellation="BearGuardian"
-            >
-              Bear-guardian
-            </button>
-            <button
-              className="btn btn-secondary"
-              onClick={this.myFunc}
-              data-constellation="Fish"
-            >
-              Fish
-            </button>
-            <button
-              className="btn btn-secondary"
-              onClick={this.myFunc}
-              data-constellation="BigBar"
-            >
-              Big-bar
-            </button>
-            <button
-              className="btn btn-secondary"
-              onClick={this.myFunc}
-              data-constellation="Fuhrmann"
-            >
-              Fuhrmann
-            </button>
+            {IMG_DATA.map((item, index) => {
+              return (
+                <button
+                  key={index}
+                  className="btn btn-secondary"
+                  onClick={this.myFunc}
+                  name={item.name}
+                  url={item.url}
+                  data-url={item.url}
+                >
+                  {item.name}
+                </button>
+              );
+            })}
           </div>
         </div>
-        <Content url={this.state.img} name={this.state.constellation} />
+        <Content url={this.state.url} name={this.state.name} />
       </div>
     );
   }
--- src/Content.js
+++ src/Content.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Content extends Component {
   render() {
     return (
-      <div className="container">
+      <div className="container content">
         <p className="text-muted">{this.props.name}</p>
         <img src={this.props.url} alt="pic" />
       </div>
