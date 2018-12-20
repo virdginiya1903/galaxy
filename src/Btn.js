1aa4a13ea0650cac66bf86d11122ab91525337b2
virdginiya1903 <virdginiya1903@mail.ru>
2018-12-20 16:45:30 +0300

    Поменяла компонент Btn с класса на функцию

--- src/Btn.js
+++ src/Btn.js
@@ -1,26 +1,41 @@
-import React, { Component, Fragment } from "react";
-import IMG_DATA from "./data";
+import React, { Fragment } from "react";
 
-class Btn extends Component {
-  render() {
+// class Btn extends Component {
+//   render() {
+//     return (
+//       <Fragment>
+//         {IMG_DATA.map((item, index) => {
+//           return (
+//             <button
+//               key={index}
+//               className="btn btn-secondary"
+//               onClick={this.props.myFunc}
+//               name={item.name}
+//               data-url={item.url}
+//             >
+//               {item.name}
+//             </button>
+//           );
+//         })}
+//       </Fragment>
+//     );
+//   }
+// }
+
+const Btn = ({ data, myFunc }) => {
+  const elements = data.map(item => {
     return (
-      <Fragment>
-        {IMG_DATA.map((item, index) => {
-          return (
-            <button
-              key={index}
-              className="btn btn-secondary"
-              onClick={this.props.myFunc}
-              name={item.name}
-              data-url={item.url}
-            >
-              {item.name}
-            </button>
-          );
-        })}
-      </Fragment>
+      <button
+        className="btn btn-secondary"
+        onClick={myFunc}
+        name={item.name}
+        data-url={item.url}
+      >
+        {item.name}
+      </button>
     );
-  }
-}
+  });
+  return <Fragment>{elements}</Fragment>;
+};
 
 export default Btn;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import Btn from "./Btn";
+import IMG_DATA from "./data";
 
 class Navbar extends Component {
   render() {
     return (
       <div className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container">
-          <Btn myFunc={this.props.myFunc} />
+          <Btn myFunc={this.props.myFunc} data={IMG_DATA} />
         </div>
       </div>
     );
